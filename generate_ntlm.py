ea5140eccfba7f53585d46dfdaf3b7c98b6767ea
FullTang <46549513+FullTang@users.noreply.github.com>
2023-04-03 05:59:11 -0600

    Accepts args OR prompts the user

--- generate_ntlm.py
+++ generate_ntlm.py
@@ -1,6 +1,10 @@
 import hashlib
+import sys
 
-input_str = input("Enter the string to convert to NTLM hash: ")
+if len(sys.argv) > 1:
+    input_str = sys.argv[1]
+else:
+    input_str = input("Enter the string to convert to NTLM hash: ")
 
 hash_obj = hashlib.new('md4', input_str.encode('utf-16le'))
 ntlm_hash = hash_obj.digest()
